37d53e7eaab3428efb906eec2ba970baf66d216f
Jose Angel De La Cruz Alonso <a01772695@tec.mx>
2026-04-20 17:32:21 -0600

    Refactor ExchangeRate module and update imports

--- monarca/src/exchange-rate/exchange-rate.module.ts
+++ monarca/src/exchange-rate/exchange-rate.module.ts
@@ -1,12 +1,15 @@
-/**
- * Módulo que registra la entidad ExchangeRa para que pueda ser utilizada por otros módulos del sistema.
- */
+// aqui solo se define el módulo de tipos de cambio, que incluye la entidad de tipo de cambio, el cliente para consultar la API de Banxico, el servicio que maneja la lógica de negocio relacionada con los tipos de cambio y el controlador que expone los endpoints para obtener esta información. 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ExchangeRate } from './exchange-rate.entity';
+import { ExchangeRate } from './entities/exchange-rate.entity';
+import { BanxicoClient } from './banxico.client';
+import { ExchangeRateService } from './exchange-rate.service';
+import { ExchangeRateController } from './exchange-rate.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ExchangeRate])],
-  exports: [TypeOrmModule],
+  controllers: [ExchangeRateController],
+  providers: [BanxicoClient, ExchangeRateService],
+  exports: [ExchangeRateService],
 })
 export class ExchangeRateModule {}
